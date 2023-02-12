6bbd3050934b74829646a18937bfd6deb594ef39
Tiago Costa <tiagodpv@gmail.com>
2023-02-12 03:09:11 -0500

    first

--- src/App.js
+++ src/App.js
@@ -1,23 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
+import { Button, Typography } from '@mui/material';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <Typography variant='h1'>This is my app</Typography>
+      <Button variant='contained'>Hello world</Button>
     </div>
   );
 }
